488824b4a3b8f0eedb7a8cd6e08266aacd698e8d
Chris Jardine <chris@keplux.com>
2022-12-01 18:34:36 -0500

    Rever Vision Tool plugin to use process.env instead of import.meta.env.

--- src/plugins/visionTool.ts
+++ src/plugins/visionTool.ts
@@ -3,7 +3,7 @@ import { visionTool } from '@sanity/vision';
 const VISION_TOOL = visionTool({
   // Note: These are both optional
   defaultApiVersion: 'v2021-10-21',
-  defaultDataset: import.meta.env.SANITY_STUDIO_DATASET,
+  defaultDataset: process.env.SANITY_STUDIO_DATASET,
 });
 
 export default VISION_TOOL;
