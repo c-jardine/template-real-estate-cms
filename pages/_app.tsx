4a88ffd43b6d5251571c93435c3eac01769ae0fd
Chris Jardine <chris@keplux.com>
2022-12-01 18:53:55 -0500

    Remove navbar, info bar, and footer from Studio pages.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,23 @@
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 import { Footer, InformationBar, Navbar } from '../src/components';
 import '../src/styles/globals.css';
 import AppWrapper from './AppWrapper';
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
   return (
     <AppWrapper>
-      <InformationBar />
-      <Navbar />
-      <Component {...pageProps} />
-      <Footer />
+      {router.asPath !== '/studio' ? (
+        <>
+          <InformationBar />
+          <Navbar />
+          <Component {...pageProps} />
+          <Footer />
+        </>
+      ) : (
+        <Component {...pageProps} />
+      )}
     </AppWrapper>
   );
 };
